145f9d34085f2bd6d59630610b3aaf19c6362712
Jenny Chau <jchau9@uci.edu>
2019-04-19 22:58:49 -0700

    populate html with php

--- dashboard.php
+++ dashboard.php
@@ -1,5 +1,15 @@
 <?php
 session_start();
+$user = "student";
+// if user is a student or a counselor
+if ($user == "student") {
+  // get respective info from the database
+  $group_num = 3;
+  $bus_num = 10;
+  $cabin_num = 15;
+  $name = "Hello";
+  $email = "test@example.com";
+}
 ?>
 <html lang="en">
   <head>
@@ -7,23 +17,30 @@ session_start();
     <link rel="stylesheet" href="css/dashboard.css">
   </head>
   <body>
+    <nav>
+      <ul class="navigation">
+        <li><a class="active" href="http://youthspiritualsummit.weebly.com">Home</a></li>
+      </ul>
+    </nav>
     <h1>Welcome to your Dashboard!</h1>
-    <div id="todos" class="box">
-      <h2>Your To Dos:</h2>
-      <p>Checklist placeholder</p>
-    </div>
-    <div class="hidden box">
-      <h2>Camp Information</h2>
-      <p>Group Number: </p>
-      <p>Bus Number: </p>
-      <p>Cabin Number: </p>
-    </div>
-    <div class="hidden box">
-      <h2>Your Information</h2>
-      <p>Name: </p>
-      <p>Email: </p>
-      <p>Password: </p>
-      <p>ETC</p>
+    <div>
+      <div id="todos" class="box">
+        <h2>Your To Dos:</h2>
+        <p>Checklist placeholder</p>
+      </div>
+      <div class="hidden box">
+        <h2>Camp Information</h2>
+        <p>Group Number: <?php echo $group_num; ?></p>
+        <p>Bus Number: <?php echo $bus_num; ?></p>
+        <p>Cabin Number: <?php echo $cabin_num; ?></p>
+      </div>
+      <div class="hidden box">
+        <h2>Your Information</h2>
+        <p>Name: <?php echo $name; ?></p>
+        <p>Email: <?php echo $email; ?></p>
+        <p>Password: </p>
+        <p>ETC</p>
+      </div>
     </div>
   </body>
 </html>
