a1a144dc72401949f2369f87be4b8f592fdb703f
Jenny Chau <jchau9@uci.edu>
2019-04-20 16:53:10 -0700

    add main.css

--- dashboard/campers.php
+++ dashboard/campers.php
@@ -1,6 +1,10 @@
+<?php
+  session_start();
+?>
 <html lang="en">
   <head>
     <title>View Campers</title>
+    <link rel="stylesheet" href="/css/main.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
   </head>
   <body>
