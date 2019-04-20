c12e64488ba624f41ed7cc6980cffcab9a541ae1
Jenny Chau <jchau9@uci.edu>
2019-04-19 22:02:19 -0700

    separate css into own folder

--- dashboard.php
+++ dashboard.php
@@ -1,18 +1,26 @@
-<?php
-
-?>
 <html lang="en">
   <head>
     <title>Youth Spiritual Summit</title>
+    <link rel="stylesheet" href="css/dashboard.css">
   </head>
   <body>
     <h1>Welcome to your Dashboard!</h1>
-    <div id="todos">
+    <div id="todos" class="box">
       <h2>Your To Dos:</h2>
-      
+      <p>Checklist placeholder</p>
     </div>
-    <div class="hidden">
+    <div class="hidden box">
       <h2>Camp Information</h2>
+      <p>Group Number: </p>
+      <p>Bus Number: </p>
+      <p>Cabin Number: </p>
+    </div>
+    <div class="hidden box">
+      <h2>Your Information</h2>
+      <p>Name: </p>
+      <p>Email: </p>
+      <p>Password: </p>
+      <p>ETC</p>
     </div>
   </body>
 </html>
