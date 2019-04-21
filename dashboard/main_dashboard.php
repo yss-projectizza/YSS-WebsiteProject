7b188707ddab4f2f38c847cd991a5d97e6131aec
emachta <esmachta@gmail.com>
2019-04-21 14:44:08 -0700

    fixed paypal button issue

--- dashboard/main_dashboard.php
+++ dashboard/main_dashboard.php
@@ -1,3 +1,4 @@
+
 <html lang="en">
   <head>
     <title>Youth Spiritual Summit</title>
@@ -104,13 +105,31 @@
 
               <div class="card">
                 <h2>Payment</h2>
-                <p>You owe: $0</p>
+                <label>You owe: $<?php echo $credit_due;?></label>
+
                 <script src="https://www.paypal.com/sdk/js?client-id=Adh5IncLIpsFfbBF32H4FpvUzM87YDJ1wLvGCb_oJvoZ5ej_MCvreSNBV3GGJgfUiyf5zaA5FRHSsluk"></script>
                 <div id="paypal-button-container"></div>
-
                 <script>
-                  paypal.Buttons().render('#paypal-button-container');
+                  paypal.Buttons({
+                    createOrder: function(data, actions) {
+                      return actions.order.create({
+                        purchase_units: [{
+                          amount: {
+                            value: <?php echo $credit_due;?>
+                          }
+                        }]
+                      });
+                    },
+                    onApprove: function(data, actions) {
+                      // Capture the funds from the transaction
+                      return actions.order.capture().then(function(details) {
+                        // Show a success message to your buyer
+                        alert('Transaction completed by ' + details.payer.name.given_name);
+                      });
+                    }
+                  }).render('#paypal-button-container');
                 </script>
+
 
               </div>
             <?php endif ?>
@@ -147,3 +166,5 @@
 	  </div>
   </body>
 </html>
+
+
