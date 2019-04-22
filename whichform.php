f7d0fb3c4398920bb35ce69cd856b069b23080db
Harry Tjahja <harry.d.tjahja@gmail.com>
2019-04-21 17:56:49 -0700

    fixed MYcreateaccount allowing under 18 people to continue (tentative)

--- whichform.php
+++ whichform.php
@@ -1,8 +1,17 @@
 <?php
 
 session_start();
+
 $redirectpagename =$_POST["user_type"];
 $_SESSION["newuserinfo"] = $_POST;
-header("Location:$redirectpagename");
+$year = substr($_SESSION["newuserinfo"]["age"],0,4);
+$age = date("Y") - $year;
+
+if($age < 18) {
+  header("Location:MYcreateaccount.php");
+}
+else{
+  header("Location:$redirectpagename");
+}
 
 ?>
