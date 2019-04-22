9942d83a4060a20c8f1bdc592f090a1a43986898
gracechoe <gracechoe925@gmail.com>
2019-04-21 22:40:29 -0700

    Updated campers page

--- dashboard/main_users/campers.js
+++ dashboard/main_users/campers.js
@@ -1,6 +1,3 @@
-function toggleInfo(evt, infoType) {
-  var bus_num = document.getElementById("bus_test").innerHTML.slice(5);
-  console.log(bus_num);
   var config = {
       apiKey: "AIzaSyDJrK2EexTLW7UAirbRAByoHN5ZJ-uE35s",
       authDomain: "yss-project-69ba2.firebaseapp.com",
@@ -10,10 +7,35 @@ function toggleInfo(evt, infoType) {
       messagingSenderId: "530416464878"
   };
 
-  //firebase.initializeApp(config);
+  firebase.initializeApp(config);
+
+function toggleInfo(evt, infoType) {
+  var bus_num = document.getElementById("bus_test").innerHTML.slice(5);
+  console.log(bus_num);
 
   // var all_names = firebase.database().ref("/users/bus_num/").equalTo(bus_num);
   // console.log(all_names);
+
+  // firebase.database().ref("/users/").equalTo("gschoe@uci,edu").on("value",function(data) {
+  //   console.log("Equal to filter: " + data.val());
+  // });
+
+  firebase.database().ref("users").orderByChild("bus_num").equalTo(63).on("value", function(snapshot) {
+    console.log(snapshot.val());
+    snapshot.forEach(function(data) {
+        console.log(data.key);
+    });
+  });
+
+  var name = "Jenny Chau";
+
+  // firebase.database().ref('users/').once('value').then(function(snapshot)
+  //     {
+  //         console.log("checking if exists");
+  //         name = (snapshot.val() && snapshot.val().first_name);
+  //         console.log(name);
+  //     }
+  // );
 
   var i, tabcontent, tablinks;
   tabcontent = document.getElementsByClassName("tabcontent");
@@ -27,7 +49,6 @@ function toggleInfo(evt, infoType) {
   document.getElementById(infoType).style.display = "block";
   evt.currentTarget.className += "active";
 
-  var name = "Jenny Chau";
   document.getElementById("data").innerHTML = name;
 
 }
