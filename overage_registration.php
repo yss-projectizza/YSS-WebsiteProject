368f8b120081fef061e31c695ad8c337fdec3c2d
emachta <esmachta@gmail.com>
2019-04-21 22:26:54 -0700

    changed db info

--- overage_registration.php
+++ overage_registration.php
@@ -345,14 +345,14 @@ session_start();
         <script src="https://www.gstatic.com/firebasejs/5.10.0/firebase-database.js"></script>
         <!--<script src="counselor_app.js"></script>-->
         <script>
-            var config = {
-                apiKey: "AIzaSyDdBVALQJWdMvR5ed0UswgmdWY1me9eL20",
-                authDomain: "inf117.firebaseapp.com",
-                databaseURL: "https://inf117.firebaseio.com",
-                projectId: "inf117",
-                storageBucket: "inf117.appspot.com",
-                messagingSenderId: "839601382632"
-            };
+                    var config = {
+                        apiKey: "AIzaSyDJrK2EexTLW7UAirbRAByoHN5ZJ-uE35s",
+                        authDomain: "yss-project-69ba2.firebaseapp.com",
+                        databaseURL: "https://yss-project-69ba2.firebaseio.com",
+                        projectId: "yss-project-69ba2",
+                        storageBucket: "yss-project-69ba2.appspot.com",
+                        messagingSenderId: "530416464878"
+                    };
             firebase.initializeApp(config);
 
             document.getElementById("submitContact").addEventListener("click", functSubmit);
