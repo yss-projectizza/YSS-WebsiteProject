368f4cccefa053de21228fbcbd9c3ec93623ac37
simeon13 <simeon.j.lam@gmail.com>
2019-04-21 17:57:27 -0700

    added forgot password

--- login.php
+++ login.php
@@ -60,7 +60,6 @@ if(  (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"])){
 					<button onclick="registerApprove()" id="register" class="btn btn-med btn-outline-info btn-block" role="button" style="margin-top:18">Register</button>
 					<script>
 						function registerApprove() {
-						var txt;
 						var age = prompt("Please enter your age:", "");
 						if (age == null) {
 							window.alert("Age not entered.");
@@ -72,24 +71,38 @@ if(  (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"])){
 						else {
 							window.alert("You must be 18 to register for an account.");
 						}
-						document.getElementById("demo").innerHTML = txt;
 						}
 					</script>
 
 					<button onclick="forgotPassword()" id="forgot" class="btn btn-med btn-outline-info btn-block" role="button" style="margin-top:10">Forgot Password?</button>
 					<script>
 						function forgotPassword() {
-						var txt;
-						var email = prompt("Please enter your email:", "12345@gmail.com");
-						if (email == null) {
-							window.alert("email not entered.");
+						var email = prompt("Please enter your email:", "abcde@gmail.com");
+						// email = email.replace(".", ',');
+						console.log(email);
+						var ref = firebase.database().ref();
+						ref.child("users").orderByChild("email").equalTo(email).once("value", snapshot => {
+							if (snapshot.exists()){
+								window.alert("A link to reset your password has been sent to " + email +".")
+							// TODO: Handle that users do exist
+							}
+							else {
+								alert('The email: (' + email + ') does not exist in the system.');
+							}
+						});
+					}	
+					
+						// var usersRef = firebase.database().ref('yss-project-69ba2');
+						// usersRef.child(email).once('value', function(snapshot) {
+						// 	var exists = (snapshot.val() !== null);
+						// 	if (exists) {
+						// 	window.alert("A link to reset your password has been sent to " + email +".")
+						// 	} else {
+						// 	alert('The email: (' + email + ') does not exist in the system.');
+						// 	}
+						// });				
+		
 
-						}
-						else {
-							window.alert("A link to reset your password has been sent to " + email +".")
-						}
-						document.getElementById("demo").innerHTML = txt;
-						}
 					</script>
 
 					<!-- <a id="forgot" class="btn btn-med btn-outline-info btn-block" href="/forgot" role="button" style="margin-top:12">Forgot Password</a> -->
