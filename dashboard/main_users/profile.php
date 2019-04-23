622b3366749f964c0332763066b147141c701b3a
gracechoe <gracechoe925@gmail.com>
2019-04-23 13:53:45 -0700

    testing profile merge

--- dashboard/main_users/profile.php
+++ dashboard/main_users/profile.php
@@ -3,7 +3,7 @@
 ?>
 <html lang="en">
   <head>
-    <title>Profile</title>
+    <title>Edit Your Account | Youth Spiritual Summit</title>
     <link rel="stylesheet" href="/css/profile.css" />
     <link rel="stylesheet" href="/css/main.css">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css" integrity="sha384-GJzZqFGwb1QTTN6wy59ffF1BuGJpLSa9DkKMp0DgiMDm4iYMj70gZWKYbI706tWS" crossorigin="anonymous">
@@ -27,14 +27,24 @@
         </div>
       </div>
     </nav>
-    <div class="container rounded box">
-    <!-- Financial Aid Information -->
+    <div class="container profile-box">
+    <!-- Profile Information -->
       <label><p style = "font-size:26px;">Profile Information</p></label>
 
       <div class="row initial-task-padding">
         <div class="col">
-          Name<b style = "color: red;">*</b>
-          <input type="text" name="name" times-label="name" class="form-control" required>
+          First name<b style = "color: red;">*</b>
+          <input id="fname"type="text" name="fname"
+          times-label="fname" class="form-control" required>
+          <br>
+        </div>
+      </div>
+
+      <div class="row initial-task-padding">
+        <div class="col">
+          Last name<b style = "color: red;">*</b>
+          <input id="lname"type="text" name="lname"
+          times-label="lname" class="form-control" required>
           <br>
         </div>
       </div>
@@ -43,27 +53,94 @@
         <div class="col">
           Email<b style = "color: red;">*</b>
           <br>
-          <input type="text" name="email" times-label="email" class="form-control" required>
+          <input id="email" type="semail" name="email"
+           times-label="email" class="form-control" required>
           <br>
         </div>
       </div>
 
+      <div class="row initial-task-padding">
+        <div class="col">
+          Phone number<b style = "color: red;">*</b>
+          <input id="phone" type="tel" name="phone"
+          times-label="phone" class="form-control" required>
+          <br>
+        </div>
+      </div>
 
       <div class="row initial-task-padding">
         <div class="col">
             Password<b style = "color: red;">*</b>
-            <input type="text" name="password" times-label="password" class="form-control" required>
+            <input id="password" type="password" name="password"
+            times-label="password" class="form-control" required>
             <br>
         </div>
       </div>
 
       <!-- Submit -->
       <div class="row margin-data" style = "padding-bottom: 50px;padding-top: 10px;" align="center">
-        <div class="col">
-          <input type="submit" class="btn-xl rounded" align="center" value="Submit" >
-        </div>
+        <div class="row margin-data"
+          style = "padding-bottom: 50px;
+               padding-top: 10px;"
+               align="center";>
+            <button onclick="location.href = '/dashboard.php'" id="back"
+              class="btn-xl" align="center" role="button"> Back
+            </button>
+            <input id="update" type="button"
+              class="btn-xl" align="center" value="Save changes" >
+       </div>
       </div>
     </div>
+
+    <!--Javascript Segment
+  -->
+  <script src="https://www.gstatic.com/firebasejs/5.10.0/firebase-app.js"></script>
+  <script src="https://www.gstatic.com/firebasejs/5.10.0/firebase-database.js"></script>
+
+  <script>
+      var config = {
+          apiKey: "AIzaSyDJrK2EexTLW7UAirbRAByoHN5ZJ-uE35s",
+          authDomain: "yss-project-69ba2.firebaseapp.com",
+          databaseURL: "https://yss-project-69ba2.firebaseio.com",
+          projectId: "yss-project-69ba2",
+          storageBucket: "yss-project-69ba2.appspot.com",
+          messagingSenderId: "530416464878"
+      };
+      firebase.initializeApp(config);
+
+      document.getElementById("update").addEventListener("click", function(){
+          var database = firebase.database();
+          //getting input data
+          var fname = document.getElementById("fname").value;
+          var lname = document.getElementById("lname").value;
+          var phone = document.getElementById("phone").value;
+          var email = document.getElementById("email").value;
+          var password = document.getElementById("password").value;
+          var emailwcharactersreplaced = email.replace(".",",");
+
+          var newPostRef = firebase.database().ref('/users/' + emailwcharactersreplaced).update({
+              first_name: fname,
+              last_name: lname,
+              phone: phone,
+              email: email,
+              password: password
+            },
+             function(error){
+                if(error) {
+                    alert("didn't go through")
+                }
+                else {
+                    var postID = newPostRef.key;
+                    window.location.replace("/dashboard.php");
+                    console.log("went to firebase");
+                // Data saved successfully!
+                }
+            });
+        //}
+      });
+  </script>
+
+
     <div class="footer top-buffer">
       <div class="container">
         <div class="row align-items-center">
