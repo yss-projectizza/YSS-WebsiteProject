bfdda1de4d7bc2b7733c48ed39cff8a88e855c3f
emachta <esmachta@gmail.com>
2019-04-26 19:15:11 -0700

    added admin functionality

--- dashboard/admin_dashboard.php
+++ dashboard/admin_dashboard.php
@@ -1,6 +1,27 @@
 <?php
-  session_start();
+      if(!isset($_SESSION))
+          session_start();
 ?>
+<script src="https://www.gstatic.com/firebasejs/5.10.0/firebase.js"></script>
+
+
+<script>
+
+
+
+// Initialize Firebase
+var config = {
+  apiKey: "AIzaSyDJrK2EexTLW7UAirbRAByoHN5ZJ-uE35s",
+  authDomain: "yss-project-69ba2.firebaseapp.com",
+  databaseURL: "https://yss-project-69ba2.firebaseio.com",
+  projectId: "yss-project-69ba2",
+  storageBucket: "yss-project-69ba2.appspot.com",
+  messagingSenderId: "530416464878"
+};
+firebase.initializeApp(config);
+
+  </script>
+
 <html lang="en">
   <head>
     <title>Youth Spiritual Summit</title>
@@ -15,6 +36,17 @@
       <div class="main-cards">
         <div class="card">
           <h3>This is admin page</h3>
+          <p id="data"></p>
+            <script>
+              firebase.database().ref('/').once('value').then(async function(snapshot) {
+                let alldata = Object.keys(snapshot.val().users);
+                let printdata = alldata.map(item=>{
+                  return '<p><a href=/admin_profile.php?name=' + item +'>' + item + '</a></p>'
+                })
+                document.getElementById("data").innerHTML = printdata;
+              });
+
+            </script>
         </div>
         <div class="card">
         </div>
