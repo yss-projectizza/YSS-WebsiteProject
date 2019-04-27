1c3dca59d82559e028b6733eb743317f1f28b891
simeon13 <simeon.j.lam@gmail.com>
2019-04-27 00:48:23 -0700

    headers added

--- header_loggedout.php
+++ header_loggedout.php
@@ -3,9 +3,8 @@
 			<a class="navbar-brand" href="http://youthspiritualsummit.weebly.com">
 				<img src="https://youthspiritualsummit.weebly.com/uploads/1/1/0/7/110732989/published/yss-logo-white_2.png" width="150" height="65" alt="">
 			</a>
-			<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-			</button>
+			<button class="navbar-toggler" align="right" type="button" >
+	        </button>
 			<div class="collapse navbar-collapse" id="navbarNavAltMarkup">
 				<div class="navbar-nav mx-auto">
 					<a class="nav-item nav-link" href="/login.php"><font color="white">Login</font></a>
