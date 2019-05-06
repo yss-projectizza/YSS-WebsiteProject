5fec8853629547e8656b08024eeca545c1df9123
Jenny Chau <jchau9@uci.edu>
2019-05-05 18:16:39 -0700

    footer alignment fix

--- header_loggedin.php
+++ header_loggedin.php
@@ -33,19 +33,15 @@
 <div class="footer top-buffer">
   <div class="container">
     <div class="row align-items-center">
-      <div class="col">
-        <a class="footerphone">
-          Call us:<br>
-          949-416-3753
-        </a>
+      <div class="col" id="left">
+        Call Us: 949-416-3753
+      </div>
+      <div class="col" id="mid">
+        YSS
       </div>
       <div class="vertline"></div>
-      <div class="col">
-      <p>YSS</p>
-      </div>
-      <div class="vertline"></div>
-      <div class="col">
-      © 2019 Youth Spiritual Summit
+      <div class="col" id="right">
+        © 2019 Youth Spiritual Summit
       </div>
     </div>
   </div>
