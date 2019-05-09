a099b7d8940b5df9673df795bf9e46bcb44a5a60
Jenny Chau <jchau9@uci.edu>
2019-05-09 14:33:41 -0700

    shift schedule to the right

--- dashboard/main_dashboard.php
+++ dashboard/main_dashboard.php
@@ -47,94 +47,89 @@
       </form>
     <?php endif ?>
     <div class="row">
-      <div class="col">
-        <div class="card">
-          <h2>Your To Dos:</h2>
-          <div class="to_do">
-            <input class="check" type="checkbox" disabled="disabled" />
-            Payment has been Recieved.
+      <div class="col-md-6">
+        <div class="row">
+          <div class="card">
+            <h2>Your To Dos:</h2>
+            <div class="to_do">
+              <input class="check" type="checkbox" disabled="disabled" />
+              Payment has been Recieved.
+            </div>
           </div>
-        </div>
-      </div>
-      <?php if ($user_type != "counselor"): ?>
-      <div class="col">
-        <div class="card">
-          <h2>Payment</h2>
-          <label>You owe: <label id="amount_owed" style='font-size:22;color:red;'>$</label></label>
-          <script
-            src="https://www.paypal.com/sdk/js?client-id=Adh5IncLIpsFfbBF32H4FpvUzM87YDJ1wLvGCb_oJvoZ5ej_MCvreSNBV3GGJgfUiyf5zaA5FRHSsluk">
-          </script>
-          <div id="paypal-button-container"></div>
-          <script>
-            paypal.Buttons({
-              createOrder: function (data, actions) {
-                return actions.order.create({
-                  purchase_units: [{
-                    amount: {
-                      value: <?php echo $credit_due;?>
-                    }
-                  }]
-                });
-              },
-              onApprove: function (data, actions) {
-                // Capture the funds from the transaction
-                return actions.order.capture().then(function (details) {
-                  // Show a success message to your buyer
-
-                  console.log(details.purchase_units[0].amount.value)
-                  let amount_payed = details.purchase_units[0].amount.value;
-                  amount_payed = amount_payed.split(".");
-                  let payed_dollar = parseInt(amount_payed[0]);
-                  let payed_cents = parseInt(amount_payed[1]);
-
-                  firebase.database().ref('/users/' + email + '/credit_due').once('value').then(
-                    async function (snapshot) {
-                      var credit_now = await parseInt(snapshot.val());
-
-
-
-                      firebase.database().ref('/users/' + email).update({
-                        credit_due: credit_now - payed_dollar
-
-                      });
-
-                      location.reload();
+          <?php if ($user_type != "parent"): ?>
+            <div class="card">
+              <h2>Camp Information</h2>
+              <p>Group Number: <?php echo $group_num; ?></p>
+              <p>Bus Number: <?php echo $bus_num; ?></p>
+              <p>Cabin Number: <?php echo $cabin_num; ?></p>
+              <br />
+              <button type="button" class="rounded"
+                onclick="document.location.href = '/dashboard/main_users/campers.php';">View Group Details
+              </button>
+            </div>
+          <?php endif ?>
+          <?php if ($user_type != "counselor"): ?>
+            <div class="card">
+              <h2>Payment</h2>
+              <label>You owe: <label id="amount_owed" style='font-size:22;color:red;'>$</label></label>
+              <script
+                src="https://www.paypal.com/sdk/js?client-id=Adh5IncLIpsFfbBF32H4FpvUzM87YDJ1wLvGCb_oJvoZ5ej_MCvreSNBV3GGJgfUiyf5zaA5FRHSsluk">
+              </script>
+              <div id="paypal-button-container"></div>
+              <script>
+                paypal.Buttons({
+                  createOrder: function (data, actions) {
+                    return actions.order.create({
+                      purchase_units: [{
+                        amount: {
+                          value: <?php echo $credit_due;?>
+                        }
+                      }]
                     });
-                });
-              }
-            }).render('#paypal-button-container');
-          </script>
-          <button type="button" class="rounded" onclick="document.location.href = 'financialaid.php';">Apply for
-            Financial Aid</button>
+                  },
+                  onApprove: function (data, actions) {
+                    // Capture the funds from the transaction
+                    return actions.order.capture().then(function (details) {
+                      // Show a success message to your buyer
+
+                      console.log(details.purchase_units[0].amount.value)
+                      let amount_payed = details.purchase_units[0].amount.value;
+                      amount_payed = amount_payed.split(".");
+                      let payed_dollar = parseInt(amount_payed[0]);
+                      let payed_cents = parseInt(amount_payed[1]);
+
+                      firebase.database().ref('/users/' + email + '/credit_due').once('value').then(
+                        async function (snapshot) {
+                          var credit_now = await parseInt(snapshot.val());
+
+
+
+                          firebase.database().ref('/users/' + email).update({
+                            credit_due: credit_now - payed_dollar
+
+                          });
+
+                          location.reload();
+                        });
+                    });
+                  }
+                }).render('#paypal-button-container');
+              </script>
+              <button type="button" class="rounded" onclick="document.location.href = 'financialaid.php';">Apply for
+                Financial Aid</button>
+            </div>
+          <?php endif ?>
         </div>
-        <br>
       </div>
-    </div>
-    <?php endif; ?>
-    <div class="row">
-      <div class="col">
+      <div class="col-md-6">
         <?php if ($user_type != "parent"): ?>
-        <div class="card">
-          <h2>Camp Information</h2>
-          <p>Group Number: <?php echo $group_num; ?></p>
-          <p>Bus Number: <?php echo $bus_num; ?></p>
-          <p>Cabin Number: <?php echo $cabin_num; ?></p>
-          <br />
-          <button type="button" class="rounded"
-            onclick="document.location.href = '/dashboard/main_users/campers.php';">View Group Details
-          </button>
-        </div>
-        <?php endif ?>
-      </div>
-      <div class="col">
-        <?php if ($user_type != "parent"): ?>
-        <div class="card">
-          <h2>Schedule</h2>
-          <p>Friday</p>
-          <p>Saturday</p>
-          <p>Sunday</p>
-          <p>ETC</p>
-        </div>
+          <div class="card">
+            <h2>Schedule</h2>
+            <p>Friday</p>
+            <p>Saturday</p>
+            <p>Sunday</p>
+            <p>ETC</p>
+          </div>
         <?php endif ?>
       </div>
     </div>
