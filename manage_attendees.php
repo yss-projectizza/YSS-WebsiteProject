0ddd830fe0e758a04ed49c77f08220584c7bba7d
Jenny Chau <jchau9@uci.edu>
2019-05-10 19:58:25 -0700

    code cleanup

--- manage_attendees.php
+++ manage_attendees.php
@@ -27,47 +27,8 @@ session_start();
       <button class="rounded" id="add-youth-btn" onclick="document.location.href = './underage_registration.php';">+ Add
         Youth Participant
       </button>
-      <!-- php -->
-      <div class="container rounded box" id="add-youth">
-        <div class="rounded box youth-info">
-          <div class="right">
-            <button class="rounded" id="edit-youth"
-              onclick="document.location.href = './editchild.php?childid=<?php echo $childid; ?>';">
-              Edit Youth Participant
-            </button>
-            <button class="rounded" id="delete-youth" onclick="deleteChildById(<?php echo $childid; ?>)">
-              Delete Youth Participant
-            </button>
-          </div>
-          <div class="left">
-            <h3><?php echo $first_name . " " . $last_name ?></h3>
-            <h4>Amount Due: <span style="color: red;">$<?php echo $price?></span></h4>
-            <h4>Credit: <span style="color: green;">$<?php echo $credit?></span></h4><br>
-            <!-- <form action="updateAdditionalPaid.php" method="post">
-          Update Credit: <input type="number" step="0.01" name="amount">
-          <input type="hidden" name="childid" value="<?php echo $childid ?>">
-          <input type="submit" value="Submit">
-        </form>  -->
-          </div>
-        </div>
-      </div>
+      <div class="container rounded box" id="add-youth"></div>
     </div>
-    <!-- php -->
-    <!-- <div class="row" style="padding-bottom:100px">
-      <div class="col" >
-        <div id="test" class="card" style="border-color:grey; height: 40%;">
-          <div class="card-body">
-            <div class="d-flex justify-content-between align-items-center">
-              <a class="card-text"></a>
-              <div class="right">
-                <a href="editchild.php?childid=<?php echo $childid; ?>" role="button" class="btn btn-sm btn-secondary">Edit Youth Participant</a>
-                <button onclick="deleteChildById(<?php echo $childid; ?>)" id="deletecamper" class="btn btn-sm btn-danger">Delete Youth Participant</button>
-              </div>
-            </div>		            
-          </div>
-        </div>
-      </div>
-    </div> -->
 </body>
 
 </html>
@@ -90,10 +51,9 @@ session_start();
   firebase.initializeApp(config);
 
   firebase.database().ref('/users').once('value').then(async function (snapshot) {
-    console.log(snapshot.val())
+    //console.log(snapshot.val())
 
     var users = Object.entries(snapshot.val());
-    console.log("this is users" + users);
 
     for (let i = 0; i < users.length; i++) {
       if (users[i][1].parent_email == parent_email) {
