6f3091ac4b913acbd995cbc71ec7b344560dc208
emachta <esmachta@gmail.com>
2019-06-03 00:59:59 -0700

    small bug fixes

--- authentication.php
+++ authentication.php
@@ -3,7 +3,8 @@
     { 
         session_start(); 
     } 
-
+    //This will display errors 
+    ini_set('display_errors', 1);
 
 require __DIR__.'/vendor/autoload.php';
 
@@ -36,7 +37,7 @@ if (array_key_exists($username,$reference)){
     if ($reference[$username]["password"] == $password){
         $_SESSION["loggedin"] = true;
         $_SESSION["queryData"] = $reference[$username];
-        header("Location:Dashboard.php");
+        header("Location:dashboard.php");
     } else {
         include "login.php";
         echo "<script type='text/javascript'>alert('Incorrect Password. Please Try Again');</script>";
