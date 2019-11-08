2a1fccd600b20fe762e2144aa17619d4b23b1fb5
jsanchez97 <jxsanche@uci.edu>
2019-11-08 10:59:19 -0800

    fixed bug that did not correctly add users to database
    users were not being added under the users table of the DB

--- formToDatabase.php
+++ formToDatabase.php
@@ -1,51 +1,38 @@
-<html>
-<body>
-  <?php
+<?php
     if(!isset($_SESSION))
     {
         session_start();
     }
+    
     require 'formHandle.php';
 
     $emailwcomma = str_replace(".",",",$_SESSION["newuserinfo"]["email"]);
     $redirectpagename = $_SESSION["newuserinfo"]["user_type"];
 
-    if($redirectpagename != "parentRegistration.php") {
+    if($redirectpagename == "counselor_registration.php") 
+    {
         $userInfo = array(
           "dob" => $_SESSION["newuserinfo"]["age"],
           "bus_num" => "N/A",
           "group_num" => "N/A",
           "cabin_num" => "N/A",
           "email" => $_SESSION["newuserinfo"]["email"],
-          "account_verified" => true
-        );
-        if($redirectpagename == "overage_registration.php") {
-          $userInfo["user_type"] = "student18";
-          $userInfo["credit_due"] = "299";
-        }
-        elseif($redirectpagename == "underage_registration.php") {
-          $userInfo["user_type"] = "student";
-          $userInfo["credit_due"] = "299";
-          $userInfo["account_verified"] = false;
-        }
-        elseif($redirectpagename == "counselor_registration.php") {
+          "account_verified" => true);
+
           $userInfo["user_type"] = "counselor";
           $userInfo["account_verified"] = false;
-        }
-
     }
-    else {
+    elseif($redirectpagename == "parentRegistration.php")
+    {
         $userInfo = array(
           "dob" => $_SESSION["newuserinfo"]["age"],
           "email" => $_SESSION["newuserinfo"]["email"],
-          "account_verified" => true
-        );
+          "account_verified" => true);
 
-        if($redirectpagename == "parentRegistration.php") {
           $userInfo["user_type"] = "parent";
           $userInfo["credit_due"] = "299";
-        }
     }
+
     if($_POST["password"] != $_POST["password2"]) {
         //header("refresh:5; url=localhost:8000/overage_registration.php");
         alertRedirect("$redirectpagename", "Retyped password must match password");
@@ -57,5 +44,4 @@
       "Your account has been created successfully. Please log in to view your dashboard.");
     }
   ?>
-</body>
-</html>
+
--- login.php
+++ login.php
@@ -4,7 +4,7 @@
 			session_start(); 
 	} 
 
-	if(  (isset($_SESSION["loggedin"]) && $_SESSION["loggedin"])){
+	if((isset($_SESSION["loggedin"]) && $_SESSION["loggedin"])){
 		header("Location: dashboard.php");
 		exit;
 
