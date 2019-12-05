0f8deeb8cfbc44682517629652f561050486c27e
jsanchez97 <jxsanche@uci.edu>
2019-12-05 13:21:10 -0800

    fixed position of login page

--- login.php
+++ login.php
@@ -30,7 +30,7 @@
 <html lang="en">
 	<head>
 		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0, user-scalable=no">
 		<!-- Original Authors: Mark Otto, Jacob Thornton, and Bootstrap contributors -->
 		<title>Sign in | Youth Spiritual Summit</title>
 		<link rel="stylesheet" href="/css/main.css">
