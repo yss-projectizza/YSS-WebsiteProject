84d05ac7eb46e1c27111296c43c975dd77800d48
blair666 <blair0820@gmail.com>
2019-12-05 13:17:53 -0800

    email parents and counselor
    when user regsiter, they will receive the email from system

---
+++ email_parents.php
@@ -0,0 +1,57 @@
+<?php
+// Updated: 12/04/2019 19:11
+	if(!isset($_SESSION))
+    {
+        session_start();
+    }
+
+  require __DIR__.'/vendor/autoload.php';
+	require __DIR__.'/vendor/PHPMailer/phpmailer/src/Exception.php';
+	require __DIR__.'/vendor/PHPMailer/phpmailer/src/PHPMailer.php';
+	require __DIR__.'/vendor/PHPMailer/phpmailer/src/SMTP.php';
+
+	use PHPMailer\PHPMailer\PHPMailer;
+	use PHPMailer\PHPMailer\Exception;
+
+	$mail = new PHPMailer(true);
+
+    if ($_GET["email"] && $_GET["reset"]=="true"){
+		try {
+
+			$mail->setFrom('youthspiritualsummit@gmail.com', 'Youth Spiritual Summit');
+			$mail->addAddress($_GET['email']);
+
+			$email = $_GET['email'];
+			$comma_email = str_replace(".",",",$email);
+
+			// send email with token
+			$mail->Subject = 'Youth Spiritual Summit: Account Registration';
+
+			// $mail->Body = 'Hi '.$studentName.', your Student Account has been created';
+			$mail->Body = "Welcome to Youth Spiritual Summit. You have created an account. The login email address for your account is the email address this email is being sent to. Please use the link below to log in. Thank you.\n
+Login page: http://www.youthspiritualsummit.com/login.php";
+				$mail->Headers = 'From: info@youthspiritualsummit.com';
+			//$mail = mail($email,$subject,$message,$headers);
+
+			$mail->isSMTP();
+			//$mail->Host='relay-hosting.secureserver.net';
+			$mail->Host='a2plcpnl0093.prod.iad2.secureserver.net';
+			$mail->SMTPAuth = TRUE;
+			$mail->SMTPSecure = 'tls';
+			$mail->Username = 'info@youthspiritualsummit.com';
+			$mail->Password = 'admin1234';
+			$mail->Port = 587;
+			$mail->send();
+			echo "Email has been sent successfully!";
+		}
+		catch (Exception $e)
+		{
+			echo "Email could not be sent. Mailer Error: {$mail->ErrorInfo}";
+		}
+
+		echo "<script>
+				window.location.replace('login.php')
+				</script>";
+		exit;
+    }
+?>
--- formToDatabase.php
+++ formToDatabase.php
@@ -3,13 +3,13 @@
     {
         session_start();
     }
-    
+
     require 'formHandle.php';
 
     $emailwcomma = str_replace(".",",",$_SESSION["newuserinfo"]["email"]);
     $redirectpagename = $_SESSION["newuserinfo"]["user_type"];
 
-    if($redirectpagename == "counselor_registration.php") 
+    if($redirectpagename == "counselor_registration.php")
     {
         $userInfo = array(
           "dob" => $_SESSION["newuserinfo"]["age"],
@@ -29,23 +29,34 @@
           "email" => $_SESSION["newuserinfo"]["email"],
           "account_verified" => true);
 
-        if($redirectpagename == "parentRegistration.php") 
+        if($redirectpagename == "parentRegistration.php")
         {
 			    $userInfo["group_num"] = "";
           $userInfo["user_type"] = "parent";
           $userInfo["credit_due"] = "0";
         }
     }
-    
-    if($_POST["password"] != $_POST["password2"]) 
+
+    if($_POST["password"] != $_POST["password2"])
     {
         alertRedirect("$redirectpagename", "Retyped password must match password");
     }
-    else 
+    else
     {
       $updateFirebase($emailwcomma, $userInfo, $_POST);
+
+      // window.location.href = 'email_parents.php?email=' + $_SESSION['newuserinfo']['email'] + '&reset=true';
+
+      // echo "<script> window.location.replace('dashboard.php') </script>";
+
+      $email = $_SESSION["newuserinfo"]["email"];
+
+      echo "<script type='text/javascript'>
+              window.location.href = 'email_parents.php?email=$email&reset=true';
+            </script>";
+
       alertRedirect("login.php",
       "Your account has been created successfully. Please log in to view your dashboard.");
-			
+
     }
 ?>
